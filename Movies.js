a9961a950bd4faeaeee57f24065b01c74ff8567e
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-11-18 18:05:46 -0500

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,11 +1,27 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList } from "react-native";
 import styles from "./styles";
+import { fetchMovies } from "./api";
 
 export default function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const fetchedMovies = await fetchMovies();
+      setMovies(fetchedMovies);
+    };
+
+    getMovies();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Movies</Text>
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.uid.toString()}
+        renderItem={({ item }) => <Text style={styles.item}>{item.properties.title}</Text>}
+      />
     </View>
   );
 }
