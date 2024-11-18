ea96212a50ac750159d1206ab2a477f4ad9b210e
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-11-18 18:06:03 -0500

    Update Planets.js

--- Planets.js
+++ Planets.js
@@ -1,11 +1,27 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList } from "react-native";
 import styles from "./styles";
+import { fetchPlanets } from "./api";
 
-export default function Planets({ navigation }) {
+export default function Planet() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const getPlanets = async () => {
+      const fetchedPlanets = await fetchPlanets();
+      setPlanets(fetchedPlanets);
+    };
+
+    getPlanets();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Planets</Text>
+      <FlatList
+        data={planets}
+        keyExtractor={(item) => item.uid.toString()}
+        renderItem={({ item }) => <Text style={styles.item}>{item.properties.name}</Text>}
+      />
     </View>
   );
 }
