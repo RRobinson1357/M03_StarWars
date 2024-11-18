7a8ac918c77d4208b4466105b7a9f3c17ff4afea
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-11-18 13:36:59 -0500

    Create Planets.js

---
+++ Planets.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { View, Text, StatusBar } from "react-native";
+
+import styles from "./styles";
+
+export default function Details({ route, navigation }) {
+
+  const { content, title } = route.params;
+
+  React.useLayoutEffect(() => {
+
+    navigation.setOptions({ title });
+
+  }, []);
+
+  return (
+
+    <View style={styles.container}>
+
+      <StatusBar barStyle="dark-content" />
+
+      <Text>{content}</Text>
+
+    </View>
+
+  );
+
+}
