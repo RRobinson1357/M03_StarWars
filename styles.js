f25810d04a13c2b79361d7e60d368937fe4412d2
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-11-18 18:06:38 -0500

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,9 +1,15 @@
 import { StyleSheet } from "react-native";
-
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    paddingTop: 40,
+  },
+  item: {
+    margin: 5,
+    padding: 5,
+    color: "slategrey",
+    backgroundColor: "ghostwhite",
+    textAlign: "center",
+  },
 });
