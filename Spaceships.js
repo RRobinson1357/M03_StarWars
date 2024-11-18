70485dcf18f9ffcea90b4733866f5c7f9625e651
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-11-18 18:06:20 -0500

    Update Spaceships.js

--- Spaceships.js
+++ Spaceships.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList } from "react-native";
 import styles from "./styles";
+import { fetchStarships } from "./api";
 
-export default function Spaceships({ navigation }) {
+export default function Spaceships() {
+  const [starships, setStarships] = useState([]);
+
+  useEffect(() => {
+    const getStarships = async () => {
+      const fetchedStarships = await fetchStarships();
+      setStarships(fetchedStarships);
+    };
+
+    getStarships();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Spaceships</Text>
+      <FlatList
+        data={starships}
+        keyExtractor={(item) => item.uid.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{item.properties.name}</Text>
+            <Text>Model: {item.properties.model}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
