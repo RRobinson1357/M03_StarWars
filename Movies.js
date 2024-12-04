585c9aa461c17696cbc46243f253d21382bc20f7
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-12-04 10:28:14 -0500

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
+import PropTypes from "prop-types";
+import { View, Text, FlatList, TextInput } from "react-native";
+import ConfirmationModal from "./ConfirmationModal";
 import styles from "./styles";
 import { fetchMovies } from "./api";
 
+function Input(props) {
+  return (
+    <View style={styles.textInputContainer}>
+      <Text style={styles.textInputLabel}>{props.label}</Text>
+      <TextInput style={styles.textInput} {...props} />
+    </View>
+  );
+}
+
+Input.propTypes = {
+  label: PropTypes.string,
+};
+
 export default function Movies() {
   const [movies, setMovies] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [inputText, setInputText] = useState("");
+
+  const handleInputSubmit = () => {
+    setModalVisible(true);
+  };
 
   useEffect(() => {
     const getMovies = async () => {
@@ -17,6 +38,23 @@ export default function Movies() {
 
   return (
     <View style={styles.container}>
+      
+      <TextInput
+        style={styles.textInput}
+        placeholder="Search"
+        value={inputText}
+        onChangeText={setInputText}
+        onSubmitEditing={handleInputSubmit}
+      />
+      <ConfirmationModal
+        animationType="fade"
+        visible={modalVisible}
+        message={inputText}
+        onPressConfirm={() => setModalVisible(false)}
+        onPressCancel={() => setModalVisible(false)}
+      />
+
+
       <FlatList
         data={movies}
         keyExtractor={(item) => item.uid.toString()}
