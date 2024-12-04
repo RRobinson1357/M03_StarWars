d31f444bf7e7cb42d6535101ebb615e7255dfeda
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-12-04 10:29:01 -0500

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,15 +1,55 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    paddingTop: 40,
-  },
-  item: {
-    margin: 5,
-    padding: 5,
-    color: "slategrey",
-    backgroundColor: "ghostwhite",
-    textAlign: "center",
-  },
+  
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 20,
+  },
+
+  item: {
+    margin: 10,
+    padding: 10,
+    color: 'black',
+    backgroundColor: 'white',
+    textAlign: 'center',
+  },
+
+  textInputContainer: {
+    marginTop: 10
+  },
+
+  textInput: {
+    backgroundColor: "#FFFFFF",
+    height: 30,
+    fontSize: 15,
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  modalInner: {
+    backgroundColor: "black",
+    padding: 20,
+    borderWidth: 5,
+    borderColor: "lightsteelblue",
+    borderRadius: 5,
+    alignItems: "center",
+  },
+
+  modalText: {
+    fontSize: 16,
+    margin: 5,
+    color: "white",
+  },
+
+  modalButton: {
+    fontWeight: "bold",
+    margin: 5,
+    color: "white",
+  },
 });
